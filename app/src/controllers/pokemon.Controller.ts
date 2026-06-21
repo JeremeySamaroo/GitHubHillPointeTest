21990eeb1fdecafe8e7521e3fe6ff89f83d778de
Jeremey Samaroo <jeremey_samaroo@sagicor.com>
2026-06-21 12:56:54 -0400

    added services, routes and controllers for pokeAPi.

---
+++ app/src/controllers/pokemon.Controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import {checkPokeApiHealth, getPokemon} from "../services/pokeapi.service";
+
+
+export async function pokeApiHealthCheck(req: Request, res: Response) {
+    const result = await checkPokeApiHealth();
+
+    if (result.status === "UP") {
+        return res.status(200).json({
+            service: "pokeapi",
+            status: "UP",
+            pokeapiStatusCode: result.pokeapiStatus,
+        });
+    }
+
+    return res.status(503).json({
+        service: "pokeapi",
+        status: "DOWN",
+        error: result.error,
+    });
+}
+export async function fetchPokemon(req: Request, res: Response) {
+    try {
+        const { name } = req.params;
+
+        const data = await getPokemon(name);
+
+        res.json({
+            name: data.name,
+            id: data.id,
+            height: data.height,
+            weight: data.weight,
+            types: data.types.map((t: any) => t.type.name),
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to fetch Pokémon",
+        });
+    }
+}
